2a9d6c804c9d6136278111659a43c2ecf614e274
Aphrora0731 <994717919@qq.com>
2020-10-21 10:50:25 +0800

    better Cprint function

--- ycyOS.c
+++ ycyOS.c
@@ -4,9 +4,13 @@
 extern int print(char* str,int len,int pos);
 extern void clear_screen();
 extern void init_sys();
-int Cprint(char* mssg,int len,int px)
+int Cprint(char* mssg,int len,int px,int py)
 {
-	print(mssg,len,px);
+	//80 charactor per line
+	//px represents column
+	//py represents row
+	int pos = px+80*py;
+	print(mssg,len,pos);
 	return 0;
 }
 
@@ -14,7 +18,7 @@ void ycyOS(int status)
 {	
 	clear_screen();
 	init_sys();
-	char mssg[10] = "0123456789";
+	char mssg[80] = "0123456789";
 	int i = 0;
 	while(1)
 	{
@@ -23,13 +27,13 @@ void ycyOS(int status)
 		if( input != '\0')
 		{
 			mssg[i] = input;
-			Cprint(&input,1,i);
+			Cprint(&input,1,i,0);
 			i++;
-			if(i == 10 )break;
-					}
+			if(i == 80 )break;
+		}
 		//Cprint(&input,1,i);
 	}
-	Cprint(mssg,10,12);
+	Cprint(mssg,80,0,1);
 	//char* mssg = "Welcome to ycyOS!";
 	//Cprint(mssg,status);
 }
